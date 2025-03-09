4813227ed04c204fc7b0ea2fdd619937d83c5335
Parthbhargav99 <74089420+Parthbhargav99@users.noreply.github.com>
2025-03-09 23:49:07 +0530

    Create BookRepository.java

---
+++ BookRepository.java
@@ -0,0 +1,5 @@
+import net.javaguides.bankingapp.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
